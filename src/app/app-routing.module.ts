9ff48311c62d32a3b70b548a17f0dc45b4881674
Amit Dubey <amit.dubey@globant.com>
2019-11-08 21:41:18 +0530

    Adds angular assignment and instructions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskPanelComponent } from './components/task-panel/task-panel.component';
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [{ path: '', component: TaskPanelComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HomeComponent } from "./home/home.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { TaskPanelComponent } from './components/task-panel/task-panel.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  entryComponents: [HomeComponent],
+  declarations: [
+    AppComponent,
+    TaskPanelComponent,
+    TaskComponent,
+    TaskCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  entryComponents: [TaskPanelComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
