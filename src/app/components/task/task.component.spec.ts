2c8205e82959f652538048d8d38d1c653680d78a
Saurabh Mahajan <saurabh_mahajan29@yahoo.co.in>
2019-11-09 16:52:46 +0530

    Globant Assignment Unit test case

--- src/app/components/task/task.component.spec.ts
+++ src/app/components/task/task.component.spec.ts
@@ -22,4 +22,16 @@ describe('TaskComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Add and Edit Task should be correct', () => {
+    fixture = TestBed.createComponent(TaskComponent);
+    const h1Ele = fixture.nativeElement.querySelector('h1');
+    expect(h1Ele.innetText).toContain('Add/Edit Task Panel');
+  });
+
+  it('Task Type combox value can not be blank', () => {
+    fixture = TestBed.createComponent(TaskComponent);
+    const sel = fixture.nativeElement.querySelector('#taskType');
+    expect(sel).toBeTruthy();
+  });
 });
