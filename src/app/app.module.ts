159fba563dd8320765b79194eb5dacab51d301b1
Amol <kumbharamol22@gmail.com>
2019-11-09 14:51:48 +0530

    1. Final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { TaskPanelComponent } from './components/task-panel/task-panel.component';
-import { TaskComponent } from './components/task/task.component';
+import { TaskComponent, AddTask } from './components/task/task.component';
 import { TaskCardComponent } from './components/task-card/task-card.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskPanelComponent,
     TaskComponent,
-    TaskCardComponent
+    TaskCardComponent,
+    AddTask
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
   ],
-  entryComponents: [TaskPanelComponent],
+  entryComponents: [TaskPanelComponent,AddTask],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
+  import { Component, OnInit, Output, EventEmitter, Inject, Input } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+export interface DialogData {
+  id:number, heading: string, description:string;
+}
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -8,7 +11,42 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class TaskComponent implements OnInit {
   @Output() emitData = new EventEmitter<string>();
 
-  constructor() {}
-
+  constructor(public dialog: MatDialog) {}
+  id: number;
+  heading: string;
+  description: string;
   ngOnInit() {}
+  addTask(): void {
+    const dialogRef = this.dialog.open(AddTask, {
+      width: '450px',
+      data: {id: this.id, heading: this.heading, description: this.description}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.heading = result;
+      console.log('The dialog was closed');
+    });
+  }
+}
+@Component({
+  selector: 'app-addtask',
+  templateUrl: 'addtask.component.html',
+})
+export class AddTask {
+
+  constructor(
+    public dialogRef: MatDialogRef<AddTask>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    }
+    @Output() datatoemit = new EventEmitter<string>();
+
+    dummydata: string = "Hi";
+
+    sendData(data: string) {
+      this.datatoemit.emit(data);
+    }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
